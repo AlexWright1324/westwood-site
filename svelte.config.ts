d15ed2d5ffb5aa22933ae4cb11fe62d3cec533db
Alex Wright <alexwright1324@gmail.com>
2026-04-14 16:03:56 +0100

    fixed

--- svelte.config.ts
+++ svelte.config.ts
@@ -7,7 +7,9 @@ const config: Config = {
 		runes: ({ filename }) => (filename.split(/[/\\]/).includes("node_modules") ? undefined : true)
 	},
 	kit: {
-		adapter: adapter({})
+		adapter: adapter({
+			fallback: "200.html"
+		})
 	}
 }
 
